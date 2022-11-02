6fbfe0c376f28c6675176942568369dac51e6ee5
James <jbradley@industriousoffice.com>
2022-11-02 10:13:20 -0400

    corrected Month/Day format. ignore dist

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./planby.cjs.production.min.js')
+  module.exports = require('./planby-jb.cjs.production.min.js')
 } else {
-  module.exports = require('./planby.cjs.development.js')
+  module.exports = require('./planby-jb.cjs.development.js')
 }
