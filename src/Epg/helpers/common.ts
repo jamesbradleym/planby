c04d2f365ef0f1de31443c547f78c0ab0ca92586
James <jbradley@industriousoffice.com>
2022-11-07 12:33:27 -0500

    .

--- src/Epg/helpers/common.ts
+++ src/Epg/helpers/common.ts
@@ -82,6 +82,7 @@ export const getTimeWidthResources = ({
     startOfTime(startDateTime, timeStep),
     timeStep
   );
+  console.log(startDateTime);
   console.log(startDateTime, startOfTime(startDateTime, timeStep), timeStep, offsetStartTimeRange);
 
   const numberOfTicksPerRange = differenceInTime(endDateTime, startDateTime, timeStep)
